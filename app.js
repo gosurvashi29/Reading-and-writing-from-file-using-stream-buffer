bd828c751a5aa511738b99ee403f077e59f79c43
gosurvashi29 <urvashigoswami350@gmail.com>
2025-02-15 20:33:34 +0530

    Fixed reading and writing from file and displaying on same screen

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require("http");
 //const fs = require("fs");
 const routes=require("./route.js")
-const server = http.createServer((routes) => {
+//routes.requestHandler();
+const server = http.createServer(routes.requestHandler)
 
-});
-
-const port = 3090;
+const port = 3070;
 server.listen(port, () => {
     console.log("Server is running on port", port);
 });
--- route.js
+++ route.js
@@ -5,21 +5,23 @@ const requestHandler=(req,res)=>{
     const url = req.url;
     const method = req.method;
 
-    if (url === '/') {
+    if (url === '/' && method=="GET") {
         res.setHeader('Content-Type', 'text/html');
         res.end(`
-            <form action="/message" method="POST">
+            <form action="/" method="POST">
                 <label>Name:</label>
                 <input type="text" name="username"></input>
                 <button type="submit">Add</button>
             </form>
+            
         `);
-    } else if (url === '/message' && method === 'POST') {
+    } else if (url === '/' && method === 'POST') {
         res.setHeader('Content-Type', 'text/html');
 
         let body = [];
         req.on('data', (chunk) => {
             body.push(chunk);
+            console.log(body);
         });
 
         req.on('end', () => {
@@ -28,15 +30,38 @@ const requestHandler=(req,res)=>{
             let formData = buffer.toString();
             console.log(formData);
             let formValue = formData.split('=')[1]; // Extract value from the form
+            formValue=formValue.replaceAll("+"," ");
             fs.writeFile("formValues.txt", formValue, (err) => {
                 if (err) {
                     console.log('Error writing to file', err);
+                    //return;
+                    res.statusCode = 302; // Redirect
+                    res.setHeader("Location", "/");
                 }
-                res.statusCode = 302; // Redirect
-                res.setHeader("Location", "/");
-                res.end();
+               
+            }
+        );
+
+        fs.readFile("formValues.txt", (err, data) => {
+            if (err) {
+                console.log('Error reading file', err);
+                res.statusCode = 500;
+                res.end('Server error');
+                return;
+            }
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`
+                <form action="/" method="POST">
+                <h1>Hello, ${data.toString()}!</h1>
+                    <label>Name:</label>
+                    <input type="text" name="username" value="${formValue}"></input>
+                    <button type="submit">Add</button>
+                    
+                </form>
+                `);
             });
         });
+        
 
     } else if (url === '/read') {
         fs.readFile("formValues.txt", (err, data) => {
@@ -55,4 +80,10 @@ const requestHandler=(req,res)=>{
     }
 }
 
-module.exports=requestHandler;
+const anotherFunction=()=>{
+    console.log("We can export more than one function or object through module.exports.")
+   }
+   module.exports={
+       requestHandler
+       //anotherFunction
+   }
